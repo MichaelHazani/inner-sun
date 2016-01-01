e7249a8ff8b3d59f8beb02f02cf2835d04c8049d
MICHAEL HAZANI <123mickey@gmail.com>
2016-01-01 15:56:16 -0800

    analog sensor, not working

--- app.js
+++ app.js
@@ -2,19 +2,34 @@ var Cylon = require('cylon');
 
 Cylon.robot({
     connections: {
-        arduino: {adaptor: 'firmata',
-        port: '/dev/tty.usbmodem1a12241'
-    }
-},
+        arduino: {
+            adaptor: 'firmata',
+            port: '/dev/tty.usbmodem1a12241'
+        }
+    },
 
-devices: {
-    led: {
-        driver: 'led',
-        pin: 13
-    }
-},
+    devices: {
+        led: {
+            driver: 'led',
+            pin: 13
+        },
 
-work: function(my) {
-    every((1).second(), my.led.toggle);
-}
+        sensor: {
+            driver: 'analog-sensor',
+            pin: '0',
+            lowerLimit: 0,
+            upperLimit: 1027
+        }
+
+    },
+
+    work: function(my) {
+        var photoVal = 0;
+
+        every((1).second(), function() {
+            // photoVal = my.sensor.analogRead();
+            // console.log('Photo cell value is ');
+            console.log("Hello, human!");
+        });
+    }
 }).start();
